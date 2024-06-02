bf956746e25368fa27b0200f675589756b07b16c
m4t124 <matias.morales.02@alu.ucm.cl>
2024-06-01 21:15:43 -0400

    login version inicial

---
+++ Front/src/AuthContext.js
@@ -0,0 +1,22 @@
+// src/AuthContext.js
+import React, { useState, createContext } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => {
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
