d87fca0af74605fd29d91fa6a9d18bc55c9cc4dd
m4t124 <matias.morales.02@alu.ucm.cl>
2024-06-01 21:50:02 -0400

    warning

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import './styles.css';
@@ -20,54 +19,43 @@ import { AuthProvider, AuthContext } from './AuthContext';
 
 function App() {
   return (
-
     <AuthProvider>
       <HashRouter>
-      <div className="App">
-        <header>
-          <div className="logo">
-            <img src={logo} alt=""/>
-          </div>
-          <nav>
-            <ul>
-              <li><a href="/">Inicio</a></li>
-              <li><a href="#catalogo">Catálogo</a></li>
-              <li><a href="#admin">Admin</a></li>
-              <li className="login"><a href="#crear-cuenta">Crear Cuenta</a></li>
-              <li className="login"><a href="#iniciar-sesion">Iniciar Sesión</a></li>
-              <li><a href="#carrito"><i className="fas fa-shopping-cart"></i></a></li>
-              <li><a href="#perfil"><i className="fas fa-user"></i></a></li>
-            </ul>
-          </nav>
-        </header>
-        <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/catalogo" element={<Catalogo />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/crear-cuenta" element={<CrearCuenta />} />
-          <Route path="/iniciar-sesion" element={<IniciarSesion />} />
-          <Route path="/carrito" element={<Carrito />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/roles" element={<Roles />} />
-          <Route path="/categoria" element={<Categoria />} />
-          <Route path="/producto" element={<Producto />} />
-          <Route path="/venta" element={<Venta />} />
-        </Routes>
-        <footer>
-          <p>&copy; 2024 Creaciones con Amor. Todos los derechos reservados.</p>
-          <div className="social-icons">
-          <a href="https://wa.me/+56975409834" target="_blank" rel="noopener noreferrer">
-            <img src={iconow} alt="Icono de WhatsApp" />
-          </a>
-          <a href="https://wa.me/+56975409834" target="_blank" rel="noopener noreferrer">
-            Contáctanos por WhatsApp
-          </a>
-          <a href="https://www.instagram.com/creaciones.con.amoor/" target="_blank" rel="noopener noreferrer">
-            <img src={icono} alt="Icono de Instagram" />
-          </a>
-          <a href="https://www.instagram.com/creaciones.con.amoor/" target="_blank" rel="noopener noreferrer">
-            Síguenos en Instagram
-          </a>
+        <div className="App">
+          <header>
+            <div className="logo">
+              <img src={logo} alt=""/>
+            </div>
+            <NavBar /> {/* Añade el componente NavBar aquí */}
+          </header>
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/catalogo" element={<Catalogo />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/crear-cuenta" element={<CrearCuenta />} />
+            <Route path="/iniciar-sesion" element={<IniciarSesion />} />
+            <Route path="/carrito" element={<Carrito />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/roles" element={<Roles />} />
+            <Route path="/categoria" element={<Categoria />} />
+            <Route path="/producto" element={<Producto />} />
+            <Route path="/venta" element={<Venta />} />
+          </Routes>
+          <footer>
+            <p>&copy; 2024 Creaciones con Amor. Todos los derechos reservados.</p>
+            <div className="social-icons">
+              <a href="https://wa.me/+56975409834" target="_blank" rel="noopener noreferrer">
+                <img src={iconow} alt="Icono de WhatsApp" />
+              </a>
+              <a href="https://wa.me/+56975409834" target="_blank" rel="noopener noreferrer">
+                Contáctanos por WhatsApp
+              </a>
+              <a href="https://www.instagram.com/creaciones.con.amoor/" target="_blank" rel="noopener noreferrer">
+                <img src={icono} alt="Icono de Instagram" />
+              </a>
+              <a href="https://www.instagram.com/creaciones.con.amoor/" target="_blank" rel="noopener noreferrer">
+                Síguenos en Instagram
+              </a>
             </div>
           </footer>
         </div>
