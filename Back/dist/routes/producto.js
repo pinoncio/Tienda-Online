cd7d54aeadff2ffae3851dfc5af33e5f602a847b
NicolasPavz <npavez43@gmail.com>
2024-06-02 17:12:45 -0400

    imagenes

--- Back/dist/routes/producto.js
+++ Back/dist/routes/producto.js
@@ -1,9 +1,24 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const multer_1 = __importDefault(require("multer"));
 const producto_1 = require("../controllers/producto");
+const path_1 = __importDefault(require("path"));
 const router = (0, express_1.Router)();
-router.post('/', producto_1.newProducto);
+const storage = multer_1.default.diskStorage({
+    destination: 'uploads/',
+    filename: (req, file, cb) => {
+        const ext = path_1.default.extname(file.originalname).toLowerCase();
+        const fileName = `${Date.now()}${ext}`;
+        cb(null, fileName);
+    }
+});
+const upload = (0, multer_1.default)({ storage: storage });
+router.post('/', upload.single('imagen'), producto_1.newProducto);
+router.patch('/:cod_producto/updateImagen', upload.single('imagen'), producto_1.updateImagen);
 router.get('/list', producto_1.getProductos);
 router.get('/:cod_producto', producto_1.getProducto);
 router.delete('/:cod_producto', producto_1.deleteProducto);
