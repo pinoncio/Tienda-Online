3638b39755ab649919872769d7e610b07e03c354
diego pino <diegopino886@gmail.com>
2024-06-25 19:43:17 -0400

    j

--- Front/src/components/ProcesandoPago.js
+++ Front/src/components/ProcesandoPago.js
@@ -1,21 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-const ProcesandoPago = () => {
-  useEffect(() => {
-    // Recuperar token_ws desde localStorage
-    const token_ws = localStorage.getItem('token_ws');
-    console.log('token_ws:', token_ws);
-
-    // Aquí puedes realizar cualquier lógica adicional con token_ws
-
-    // Limpiar localStorage cuando ya no lo necesites
-    localStorage.removeItem('token_ws');
-  }, []);
-
+const ProcesandoPago = ({ status }) => {
   return (
     <div>
-      <h2>Procesando Pago</h2>
-      <p>Espere un momento mientras se procesa su pago.</p>
+      <h2>Resultado del Pago</h2>
+      <p>{status}</p>
     </div>
   );
 };
