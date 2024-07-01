fd840aae2cf0449ff0ae6112962fea651f69629a
diego pino <diegopino886@gmail.com>
2024-07-01 17:23:24 -0400

    Revert "manejo de erroes"

--- Front/src/services/producto.js
+++ Front/src/services/producto.js
@@ -2,67 +2,13 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:3000/api/producto';
 
-export const getProductos = async () => {
-  try {
-    const response = await axios.get(`${API_URL}/list`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching productos:', error);
-    throw error;
-  }
-};
+export const getProductos = () => axios.get(`${API_URL}/list`);
 
-export const createProducto = async (producto) => {
-  try {
-    if (!producto || typeof producto !== 'object') {
-      throw new Error('producto must be a valid object');
-    }
-    const response = await axios.post(API_URL, producto);
-    return response.data;
-  } catch (error) {
-    console.error('Error creating producto:', error);
-    throw error;
-  }
-};
+export const createProducto = (producto) => axios.post(API_URL, producto);
 
-export const updateProducto = async (cod_producto, producto) => {
-  try {
-    if (!cod_producto) {
-      throw new Error('cod_producto is required');
-    }
-    if (!producto || typeof producto !== 'object') {
-      throw new Error('producto must be a valid object');
-    }
-    const response = await axios.put(`${API_URL}/${cod_producto}`, producto);
-    return response.data;
-  } catch (error) {
-    console.error(`Error updating producto with cod_producto ${cod_producto}:`, error);
-    throw error;
-  }
-};
+export const updateProducto = (cod_producto, producto) => axios.put(`${API_URL}/${cod_producto}`, producto);
 
-export const deleteProducto = async (cod_producto) => {
-  try {
-    if (!cod_producto) {
-      throw new Error('cod_producto is required');
-    }
-    const response = await axios.delete(`${API_URL}/${cod_producto}`);
-    return response.data;
-  } catch (error) {
-    console.error(`Error deleting producto with cod_producto ${cod_producto}:`, error);
-    throw error;
-  }
-};
+export const deleteProducto = (cod_producto) => axios.delete(`${API_URL}/${cod_producto}`);
 
-export const getProducto = async (cod_producto) => {
-  try {
-    if (!cod_producto) {
-      throw new Error('cod_producto is required');
-    }
-    const response = await axios.get(`${API_URL}/${cod_producto}`);
-    return response.data;
-  } catch (error) {
-    console.error(`Error fetching producto with cod_producto ${cod_producto}:`, error);
-    throw error;
-  }
-};
+export const getProducto = (cod_producto) => axios.get(`${API_URL}/${cod_producto}`);
+
